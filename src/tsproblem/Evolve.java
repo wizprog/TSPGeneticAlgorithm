31896f7e41d7ae7d123b9b4d065f8ed87b2f99f7
Marko Babic <wizprogbg@gmail.com>
2018-09-01 01:14:51 +0200

    Start

---
+++ src/tsproblem/Evolve.java
@@ -0,0 +1,22 @@
+package tsproblem;
+
+import java.awt.Frame;
+
+public class Evolve extends Frame {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Evolve() {
+		setSize(500,500);
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
